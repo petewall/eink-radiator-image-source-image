e1d551990116f6626182049718ec0f3705f9d267
Pete Wall <pete@petewall.net>
2022-11-22 22:09:04 -0600

    Improve tests and readme
    Signed-off-by: Pete Wall <pete@petewall.net>

--- README.md
+++ README.md
@@ -5,25 +5,57 @@
 Generates an image from an existing image.
 
 ```bash
-image --config config.json --height 300 --width 400
+image generate --config config.json --height 300 --width 400
 ```
 
 ## Configuration
 
 The configuration is the image source, which must be a publically accessible URL, the scaling algorithm (see below) and the background color (if required).
 
-```json
-{
-    "src": "http://example.com/surfing.jpg",
-    "scale": "cover",
-    "background": {
-        "color": "purple"
-    }
-}
-```
+| field            | default | required | description |
+|------------------|---------|----------|-------------|
+| source           |         | Yes      | The URL to the image |
+| scale            |         | Ye       | Algorithm to use when resizing the image to the desired resolution |
+| background.color | white   | No       | The color of the background (used when contained images are a different resolution ratio) |
 
 Possible options for `scale`:
 
 * `resize` - Resize the image to fit the desired resolution. May lead to distortions.
 * `contain` - Resize the image so the whole image fits inside the new resolution. May show some background, which will use the `background.color` configuration.
 * `cover` - Resize the image so the smallest dimension fits inside the new resolution frame. May crop out some of the original image.
+
+## Examples
+
+### A resized image
+
+```yaml
+---
+source: https://github.com/petewall/eink-radiator-image-source-image/raw/main/test/dog1.jpg
+scale: resize
+```
+
+![An image that has been resized from the original](test/outputs/resize.png)
+
+### A covered image
+
+```yaml
+---
+source: https://github.com/petewall/eink-radiator-image-source-image/raw/main/test/dog3.jpg
+scale: cover
+```
+
+![An image that has been scaled so it fits the new size](test/outputs/cover.png)
+
+### A contained image
+
+```json
+{
+    "source": "https://github.com/petewall/eink-radiator-image-source-image/raw/main/test/dog2.jpg",
+    "scale": "contain",
+    "background": {
+        "color": "green"
+    }
+}
+```
+
+![An image that has been scaled so its contained in the new size](test/outputs/contain.png)
--- test/generate_test.go
+++ test/generate_test.go
@@ -22,13 +22,13 @@ var _ = Describe("Generate", func() {
 	Context("using resize", func() {
 		It("generates an image that is resized to fit the desired resolution", func() {
 			outputFile = "resized.png"
-			Run("generate --config resize-config.yaml --output resized.png --height 300 --width 400")
+			Run("generate --config inputs/resize.yaml --output " + outputFile + " --height 300 --width 400")
 			Eventually(CommandSession).WithTimeout(time.Second * 5).Should(Exit(0))
 
 			By("saving the image to a file", func() {
 				actualData, err := os.ReadFile(outputFile)
 				Expect(err).ToNot(HaveOccurred())
-				expectedData, err := os.ReadFile("expected_resized.png")
+				expectedData, err := os.ReadFile("outputs/resize.png")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(actualData).To(Equal(expectedData))
 			})
@@ -38,13 +38,13 @@ var _ = Describe("Generate", func() {
 	Context("using contain", func() {
 		It("generates an image that is shrunk to fit the desired resolution", func() {
 			outputFile = "contained.png"
-			Run("generate --config contain-config.json --output contained.png --height 300 --width 300")
+			Run("generate --config inputs/contain.json --output " + outputFile + " --height 300 --width 300")
 			Eventually(CommandSession).WithTimeout(time.Second * 5).Should(Exit(0))
 
 			By("saving the image to a file", func() {
 				actualData, err := os.ReadFile(outputFile)
 				Expect(err).ToNot(HaveOccurred())
-				expectedData, err := os.ReadFile("expected_contained.png")
+				expectedData, err := os.ReadFile("outputs/contain.png")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(actualData).To(Equal(expectedData))
 			})
@@ -54,13 +54,13 @@ var _ = Describe("Generate", func() {
 	Context("using cover", func() {
 		It("generates an image that is cropped to fit the desired resolution", func() {
 			outputFile = "covered.png"
-			Run("generate --config cover-config.yaml --output covered.png --height 300 --width 400")
+			Run("generate --config inputs/cover.yaml --output " + outputFile + " --height 300 --width 400")
 			Eventually(CommandSession).WithTimeout(time.Second * 5).Should(Exit(0))
 
 			By("saving the image to a file", func() {
 				actualData, err := os.ReadFile(outputFile)
 				Expect(err).ToNot(HaveOccurred())
-				expectedData, err := os.ReadFile("expected_covered.png")
+				expectedData, err := os.ReadFile("outputs/cover.png")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(actualData).To(Equal(expectedData))
 			})
@@ -69,10 +69,10 @@ var _ = Describe("Generate", func() {
 
 	When("using --to-stdout", func() {
 		It("writes the image to stdout", func() {
-			Run("generate --config resize-config.yaml --to-stdout --height 300 --width 400")
+			Run("generate --config inputs/resize.yaml --to-stdout --height 300 --width 400")
 			Eventually(CommandSession).WithTimeout(time.Second * 5).Should(Exit(0))
 
-			expectedData, err := os.ReadFile("expected_resized.png")
+			expectedData, err := os.ReadFile("outputs/resize.png")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(CommandSession.Out.Contents()).To(Equal(expectedData))
 		})
